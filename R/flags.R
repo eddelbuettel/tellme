9579e9f1a6e382211ee26ff834d97ab0a17d3d6e
Dirk Eddelbuettel <edd@debian.org>
2026-07-29 15:51:15 -0500

    Add 'everything()' wrapper

--- R/flags.R
+++ R/flags.R
@@ -7,3 +7,16 @@
 flags <- function() {
     c(arm64 =  grepl("aarch", R.version$platform))
 }
+
+##' @title everything
+##'
+##' @description Call available functions
+##'
+##' @return Nothing, function invoked for side effects
+##' @export
+everything <- function() {
+    print(tellme())
+    print(versions())
+    print(flags())
+    invisible()
+}
